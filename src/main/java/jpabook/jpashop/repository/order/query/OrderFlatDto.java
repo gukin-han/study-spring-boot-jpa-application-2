fdef46dcf4aa48da886e9a1226729be6e9d4631c
Gukin Han <gukin.dev@gmail.com>
2024-01-19 15:21:03 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화 end

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository.order.query;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderFlatDto {
+
+    private Long OrderId;
+    private String name;
+    private LocalDateTime OrderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, String itemName, int orderPrice, int count) {
+        OrderId = orderId;
+        this.name = name;
+        OrderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
